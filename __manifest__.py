01a1342999408ca27ee45d4af72b25e186cf5f28
daif2468 <daif08917@gmail.com>
2026-09-16 13:58:35 +0300

    Fix manifest dependencies and data file list

--- __manifest__.py
+++ __manifest__.py
@@ -14,29 +14,27 @@
     'depends': [
         'sale',
         'account',
+        'stock',
     ],
     'data': [
-	    'views/sale_order_views.xml',
+        'views/sale_order_views.xml',
         'views/stock_picking_views.xml',
-	    'views/account_move_view.xml',
-        'report/paperformat.xml'
-	    'report/report_invoice_internal_ref.xml',
+        'views/account_move_view.xml',
+        'report/paperformat.xml',
+        'report/report_invoice_internal_ref.xml',
         'report/report_saleorder_barcode.xml',
         'report/report_saleorder_delivery.xml',
         'report/report_saleorder_internal_ref.xml',
-
-	
     ],
-'assets': { 
-    'web.assets_backend': [ 
-            'new_reports/static/src/css/report_fonts.css', 
-        ], 
-        'web.report_assets_common': [ 
-            'new_reports/static/src/css/report_fonts.css', 
-        ], 
+    'assets': {
+        'web.assets_backend': [
+            'new_reports/static/src/css/report_fonts.css',
+        ],
+        'web.report_assets_common': [
+            'new_reports/static/src/css/report_fonts.css',
+        ],
     },
-
-    "installable": True,
-    "application": True,
-    "license": "AGPL-3",
+    'installable': True,
+    'application': True,
+    'license': 'AGPL-3',
 }
